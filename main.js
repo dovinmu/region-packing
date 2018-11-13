eaef82c7dfafe498c9cab66ae9120493d1e8e8ec
Colin <tresscd@gmail.com>
2018-11-13 17:10:34 -0500

    Get north america on the page.

--- main.js
+++ main.js
@@ -1,7 +1,63 @@
 
 let svg = d3.select("body").append("svg");
 
-let text = svg.append('text')
-    .attr('x', 20)
-    .attr('y', 20)
-    .text("hi world");
+var width = 700,
+    height = 580;
+
+var albersProjection = d3.geoAlbers()
+        .scale(100)
+        .rotate([0, 0, -30])
+        // .center([0, -38.4161])
+        .translate([width/2, height/2]);
+
+var geoPath = d3.geoPath()
+    .projection(albersProjection);
+
+
+
+d3.json('world.geo.json/countries/USA.geo.json').then(function(data) {
+  svg.selectAll('.country')
+      .data(data.features)
+      .enter()
+      .append('path')
+      .attr('d', geoPath)
+      .attr('fill', 'purple');
+
+});
+let canada;
+d3.json('world.geo.json/countries/CAN.geo.json').then(function(data) {
+  canada = svg.selectAll('.country')
+      .data(data.features)
+      .enter()
+      .append('path')
+      .attr('d', geoPath)
+      .attr('fill', 'red')
+});
+
+d3.json('world.geo.json/countries/ARG.geo.json').then(function(data) {
+  svg.selectAll('.country')
+      .data(data.features)
+      .enter()
+      .append('path')
+      .attr('d', geoPath)
+      .attr('fill', 'green');
+
+});
+
+d3.json('world.geo.json/countries/BRA.geo.json').then(function(data) {
+  svg.selectAll('.country')
+      .data(data.features)
+      .enter()
+      .append('path')
+      .attr('d', geoPath)
+      .attr('fill', 'green');
+
+});
+
+setTimeout(function(){
+  console.log('KK');
+  albersProjection.scale(200);
+  geoPath = d3.geoPath()
+      .projection(albersProjection);
+  // canada.attr('d', geoPath);
+}, 2000)
